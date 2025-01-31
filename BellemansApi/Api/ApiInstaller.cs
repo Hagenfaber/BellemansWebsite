e2b458b82e9d8a2915767f04894914d9fe1f197b
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-01-31 22:09:18 +0100

    basic setup

---
+++ BellemansApi/Api/ApiInstaller.cs
@@ -0,0 +1,24 @@
+using Api.Infrastructure.Translations;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Api.Translations;
+
+namespace Api;
+
+/// <summary>
+/// Installer for the API layer
+/// </summary>
+public static class ApiInstaller
+{
+  /// <summary>
+  /// Register all API services
+  /// </summary>
+  /// <param name="services"></param>
+  /// <returns></returns>
+  public static IServiceCollection AddApi(this IServiceCollection services)
+  {
+    // register translator, permission service, envelope factory?, mappers, ....
+    services.AddSingleton<ITranslator, Translator>();
+
+    return services;
+  }
+}
