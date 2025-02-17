949ee269490e907ed51f839e3dffc1e523e0fe0e
joren vanGoethem <jorenvangoethem@hotmail.com>
2025-02-17 17:45:23 +0100

    get all pages from db and create navigation bar from them

--- frontend/src/components/layout/NavItem.tsx
+++ frontend/src/components/layout/NavItem.tsx
@@ -1,15 +1,17 @@
 import {FC, ReactNode} from "react";
 import {Link} from "@tanstack/react-router";
 import * as React from "react";
+import { cn } from "@/lib/utils";
 
 interface Props {
     children?: ReactNode | string;
     url?: string;
+    className?: string;
 }
 
-export const NavItem = ({ children, url }: Props) => {
+export const NavItem = ({ children, url, className }: Props) => {
     return (
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href={url}>
+        <Link className={cn("text-sm font-medium hover:underline underline-offset-4", className)} href={url}>
             {children}
         </Link>
     )
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-import {Footer} from "@/components/layout/Footer";
-import {Navigation} from "@/components/layout/Navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Footer } from "@/components/layout/Footer";
+import { Navigation } from "@/components/layout/Navigation";
 
 const queryClient = new QueryClient()
 
@@ -15,16 +15,16 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-        <QueryClientProvider client={queryClient}>
-            <div className="flex flex-col min-h-screen">
-                <Navigation />
-                <main className="flex-1">
-                    <Outlet/>
-                </main>
-                <Footer />
-            </div>
-            <ReactQueryDevtools initialIsOpen={false} position={"bottom"}/>
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex flex-col min-h-screen">
+          <Navigation/>
+          <main className="flex-1">
+            <Outlet/>
+          </main>
+          <Footer/>
+        </div>
+        <ReactQueryDevtools initialIsOpen={false} position={"bottom"}/>
+      </QueryClientProvider>
     </>
-)
+  )
 }
