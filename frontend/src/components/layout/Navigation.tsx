af02d64f0256e563d3251f1e7c9639921eab629f
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2025-02-17 20:16:20 +0100

    get all pages from db and create navigation bar from them (#12)
    * get all pages from db and create navigation bar from them
    
    * dynamic creation of nav items
    
    * update versions

--- frontend/src/components/layout/Navigation.tsx
+++ frontend/src/components/layout/Navigation.tsx
@@ -1,22 +1,26 @@
-import {Link} from "@tanstack/react-router";
-import {MountainIcon} from "@/components/Icons/Mountain";
+import { Link } from "@tanstack/react-router";
+import { MountainIcon } from "@/components/Icons/Mountain";
 import * as React from "react";
-import {NavItem} from "@/components/layout/NavItem";
-
+import { NavItem } from "@/components/layout/NavItem";
+import { usePagesGetAll } from "@/endpoints/bellemansComponents";
+import { LoadingIndicator } from "@/components/ui/loadingIndicator";
 
 export const Navigation = () => {
-    return (
-        <header className="px-4 lg:px-6 h-14 flex items-center">
-            <Link className="flex items-center justify-center" href="#">
-                <MountainIcon className="h-6 w-6"/>
-                <span className="sr-only">Build your Edge</span>
-            </Link>
-            <nav className="ml-auto flex gap-4 sm:gap-6">
-                <NavItem url={"#"}>Diensten</NavItem>
-                <NavItem url={"#"}>Over Mij</NavItem>
-                <NavItem url={"#"}>Blog</NavItem>
-                <NavItem url={"#"}>Contact</NavItem>
-            </nav>
-        </header>
-    )
+  const {data: pages, isFetching} = usePagesGetAll({});
+
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center">
+      <Link className="flex items-center justify-center" to="/">
+        <MountainIcon className="h-6 w-6"/>
+        <span className="sr-only">Build your Edge</span>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        <NavItem url={"/#"}>Diensten</NavItem>
+        {isFetching && <LoadingIndicator/>}
+        {pages && pages.pages?.map((page) => (
+          <NavItem url={`/${page.name}`} key={page.id} className={"capitalize"}>{page.name}</NavItem>
+        ))}
+      </nav>
+    </header>
+  )
 }
