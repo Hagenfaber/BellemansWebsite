b82e93e2e878d0c7dd175d082a40778120fd2a58
Jonahas <jonas@hagenfaber.eu>
2025-02-22 14:11:31 +0100

    make videos more performant and better in carousel

--- frontend/src/components/media/VideoCarousel.tsx
+++ frontend/src/components/media/VideoCarousel.tsx
@@ -1,36 +1,55 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 const VideoCarousel = ({ videos, speed = 50 }) => {
-    const containerRef = useRef(null);
-    const [videoList, setVideoList] = useState([...videos, ...videos]);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const motionRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setVideoList((prev) => [...prev.slice(1), prev[0]]);
-        }, speed * 1000);
-        return () => clearInterval(interval);
-    }, [speed]);
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (!entry.isIntersecting && entry.boundingClientRect.left < 0) {
+                    const newVideoEntry = document.createElement("video");
+                    newVideoEntry.src = (entry.target as HTMLVideoElement).src;
+                    newVideoEntry.muted = true;
+                    newVideoEntry.autoplay = true;
+                    newVideoEntry.loop = true;
+                    newVideoEntry.id = "video-" + window.crypto.randomUUID();
+                    newVideoEntry.className = "h-full";
+                    observer.observe(newVideoEntry);
+                    motionRef.current?.appendChild(newVideoEntry);
+                    observer.unobserve(entry.target);
+                }
+            },
+            { threshold: 0 }
+        );
+
+        // @ts-ignore
+        const videoEls = containerRef.current?.querySelectorAll("video");
+        videoEls?.forEach((video) => observer.observe(video));
+
+        return () => observer.disconnect();
+    }, [videos]);
 
     return (
-        <div
-            ref={containerRef}
-            className="overflow-hidden flex w-full h-full absolute top-0 left-0 -z-10"
-        >
+        <div ref={containerRef} className="absolute top-0 left-0 overflow-hidden flex w-full h-full -z-10">
             <motion.div
+                ref={motionRef}
                 className="flex h-full"
-                initial={{ x: `-${parseInt(`${Math.random() * 100}`)}%` }}
+                initial={{ x: 0 }}
                 animate={{ x: "-1000%" }}
                 transition={{ ease: "linear", duration: speed, repeat: Infinity }}
             >
                 <>
-                    {videoList.map((video, index) => (
+                    {videos.map((video, index) => (
                         <video
                             key={index}
                             src={video}
                             muted
                             autoPlay
                             loop
+                            className="h-full w-auto max-w-[2000px]"
+                            id={`video-${window.crypto.randomUUID()}`}
                         />
                     ))}
                 </>
